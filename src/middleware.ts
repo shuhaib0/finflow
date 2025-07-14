ed8b86dfc877ec6d183320bdf00f0eec7cf56bb6
shuaib machingal0 <shuaibmachingal0@gmail.com>
2025-07-14 09:18:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/middleware.ts
+++ src/middleware.ts
@@ -42,6 +42,7 @@ export async function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
+// See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
